5808240c0e49af6f6e59e0092cf136f824dc8452
lianyunpeng <lianyunpeng@aiyunxiao.com>
2020-01-17 15:10:18 +0800

    自定义配置项，新增demo

---
+++ demo/ckeditor/config.js
@@ -0,0 +1,47 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	// config.extraPlugins = ['timestamp', 'abbr'];
+	// 启用插件
+	config.extraPlugins = 'uploads';
+
+	// 上传URL，该 URL 规范遵循 CKEditor 图片上传规范
+	config.filebrowserBrowseUrl = 'http://127.0.0.1:8110/system/upload';
+	config.filebrowserUploadUrl = 'https://jsonplaceholder.typicode.com/posts/';
+
+};
